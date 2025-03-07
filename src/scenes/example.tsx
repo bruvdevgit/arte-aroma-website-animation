fdc135c984148694c202344c9c9dce0cafc57139
Haxwell <thehaxwell@proton.me>
2025-03-07 20:48:23 +0200

    added the hero composition

--- src/scenes/example.tsx
+++ src/scenes/example.tsx
@@ -6,6 +6,7 @@ import batteryIcon from '/src/icons/icons8-charged-battery-100.png';
 import networkSignalIcon from '/src/icons/icons8-signal-100.png';
 import wifiSignalIcon from '/src/icons/icons8-wi-fi-100.png';
 import httpsLockIcon from '/src/icons/icons8-lock.svg';
+import heroComposition from '/src/images/hero-composition.svg';
 
 export default makeScene2D(function*(view) {
   // Create your animations here
@@ -28,14 +29,17 @@ export default makeScene2D(function*(view) {
 
       <Img src={httpsLockIcon} width={18} position={[-63, 362]} />
 
-      <Rect size={[432, 154]} position={[0, 406]} fill={'#3c3c3c'} opacity={0.7} />
-      <Txt fontFamily={'SF Pro Text'} position={[15, 362]} fontSize={18} fill={'white'}>theartearoma.com</Txt>
+      <Img src={heroComposition} width={460} position={[20, -72]} />
     </Rect>
 
+    {/*
     <Circle ref={circle} size={320} fill={'lightseagreen'} />
+    */}
 
+    <Rect size={[432, 154]} position={[0, 406]} fill={'#3c3c3c'} opacity={0.7} />
+    <Txt fontFamily={'SF Pro Text'} position={[15, 362]} fontSize={18} fill={'white'}>theartearoma.com</Txt>
     <Img src={iPhone11Mockup} height={1000} />
   </>);
 
-  yield* circle().scale(2, 2).to(1, 2);
+  //yield* circle().scale(2, 2).to(1, 2);
 });
