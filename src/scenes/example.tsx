f5ae288c96082727005a651400ec3f7a72f12521
Haxwell <thehaxwell@proton.me>
2025-03-10 13:57:41 +0200

    building browser ui

--- src/scenes/example.tsx
+++ src/scenes/example.tsx
@@ -7,6 +7,17 @@ import networkSignalIcon from '/src/icons/icons8-signal-100.png';
 import wifiSignalIcon from '/src/icons/icons8-wi-fi-100.png';
 import httpsLockIcon from '/src/icons/icons8-lock.svg';
 import heroComposition from '/src/images/hero-composition.svg';
+import prevIcon from '/src/icons/prev-icon.svg';
+import nextIcon from '/src/icons/next-icon.svg';
+import shareIcon from '/src/icons/share-icon.svg';
+import bookmarkIcon from '/src/icons/bookmark-icon.svg';
+import copyIcon from '/src/icons/copy-icon.svg';
+import tabsIcon from '/src/icons/tabs-icon.svg';
+
+// example of how box shadows are drawn
+//
+//<Rect size={[432, 154]} position={[-14, 770]} fill={'#3c3c3c'} opacity={1}
+//  shadowBlur={10} shadowColor={'red'} shadowOffset={[0, 12]} />
 
 export default makeScene2D(function*(view) {
   // Create your animations here
@@ -14,32 +25,59 @@ export default makeScene2D(function*(view) {
   const circle = createRef<Circle>();
 
   view.add(<>
-    <Rect height={1080} width={1920} fill={'#3c3c3c'} />
+    <Rect height={2100} width={3733} fill={'#3c3c3c'} />
 
     {/*
-    <Img src={iPhoneSafariExample} height={940} />
+    <Img src={iPhoneSafariExample} height={1792} />
     */}
 
     {/* browser window */}
-    <Rect height={940} width={440} fill={'#ffffff'}>
-      <Txt fontFamily={'SF Pro Text'} position={[-165, -444]} fontSize={16}>4:47</Txt>
-      <Img src={networkSignalIcon} width={14} position={[136, -445]} />
-      <Img src={wifiSignalIcon} width={18} position={[158, -445]} />
-      <Img src={batteryIcon} width={30} position={[185, -445]} />
+    <Rect height={1792} width={828} fill={'#ffffff'}>
+      {/*
+    <Rect height={1792} width={828}>
+    */}
+      <Txt fontFamily={'SF Pro Text'} position={[-316, -848]} fontSize={34}>4:47</Txt>
+      <Img src={networkSignalIcon} width={28} position={[258, -848]} />
+      <Img src={wifiSignalIcon} width={30} position={[302, -846]} />
+      <Img src={batteryIcon} width={60} position={[353, -849]} />
 
-      <Img src={httpsLockIcon} width={18} position={[-63, 362]} />
+      <Img src={httpsLockIcon} width={29} position={[-122, 690]} />
 
       <Img src={heroComposition} width={460} position={[20, -72]} />
     </Rect>
 
-    {/*
     <Circle ref={circle} size={320} fill={'lightseagreen'} />
+
+
+    <Rect height={267} width={828} fill={'#3c3c3c'}
+      position={[0, 762]} opacity={0.8} />
+    <Rect height={90} width={80} fill={'#3c3c3c'} position={[-413, 689]} opacity={0.3}
+      radius={25} />
+    <Rect height={90} width={717} fill={'#3c3c3c'} position={[0, 689]} opacity={0.3}
+      radius={25}
+    />
+    {/*
     */}
 
-    <Rect size={[432, 154]} position={[0, 406]} fill={'#3c3c3c'} opacity={0.7} />
-    <Txt fontFamily={'SF Pro Text'} position={[15, 362]} fontSize={18} fill={'white'}>theartearoma.com</Txt>
-    <Img src={iPhone11Mockup} height={1000} />
+
+    <Rect height={10} width={294} fill={'white'}
+      position={[0, 875]}
+      radius={25} />
+    {/*
+    */}
+
+    {/* browser UI chin */}
+    <Img src={prevIcon} width={22} position={[-356, 784]} />
+    <Img src={nextIcon} width={22} position={[-176, 784]} />
+    <Img src={shareIcon} width={38} position={[-4, 783]} />
+    <Img src={bookmarkIcon} width={47} position={[170, 784]} />
+    <Img src={copyIcon} width={44} position={[346, 784]} />
+    <Img src={tabsIcon} width={33} position={[-306, 689]} />
+
+
+    <Txt fontFamily={'SF Pro Text'} position={[22, 688]} fontSize={34} fill={'white'}>theartearoma.com</Txt>
+    <Img src={iPhone11Mockup} height={1905} />
   </>);
 
-  //yield* circle().scale(2, 2).to(1, 2);
+  yield* circle().scale(2, 2).to(1, 2);
 });
