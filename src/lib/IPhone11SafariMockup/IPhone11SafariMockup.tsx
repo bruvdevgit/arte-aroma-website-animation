2d144b6bb887b26a394a4149cbbbd00a64677061
Haxwell <thehaxwell@proton.me>
2025-03-11 12:31:51 +0200

    implemented IPhoneSafariMockup component and abstracted out the code for it

---
+++ src/lib/IPhone11SafariMockup/IPhone11SafariMockup.tsx
@@ -0,0 +1,88 @@
+import { ColorSignal, PossibleColor, SignalValue } from '@motion-canvas/core';
+import {
+	Circle,
+	Img,
+	Node,
+	NodeProps,
+	Rect,
+	Txt,
+	colorSignal,
+	initial,
+	signal,
+} from '@motion-canvas/2d';
+
+
+import iPhone11Mockup from '/src/images/iphone-mockup.png';
+
+import timeSVG from './icons/status-bar/TimeStyle.svg';
+import batteryIcon from './icons/status-bar/Battery.svg';
+import networkSignalIcon from './icons/status-bar/CellularConnection.svg';
+import wifiSignalIcon from './icons/status-bar/Wifi.svg';
+
+import httpsLockIcon from './icons/icons8-lock.svg';
+import prevIcon from './icons/prev-icon.svg';
+import nextIcon from './icons/next-icon.svg';
+import shareIcon from './icons/share-icon.svg';
+import bookmarkIcon from './icons/bookmark-icon.svg';
+import copyIcon from './icons/copy-icon.svg';
+import tabsIcon from './icons/tabs-icon.svg';
+
+export interface IPhone11SafariMockupProps extends NodeProps {
+	statusBarColor?: SignalValue<PossibleColor>;
+}
+
+export class IPhone11SafariMockup extends Node {
+	@initial('white')
+	@colorSignal()
+	public declare readonly statusBarColor: ColorSignal<this>;
+
+	public constructor(props?: IPhone11SafariMockupProps) {
+		super({
+			...props,
+		});
+
+		this.add(<>
+
+			{/*
+			*/}
+			<Rect fill={this.statusBarColor}
+				size={[828, 96]} position={[0, -848]} />
+
+			<Img src={timeSVG} width={120} position={[-315, -849]} />
+			<Img src={networkSignalIcon} width={39} position={[257, -846]} />
+			<Img src={wifiSignalIcon} width={35} position={[301, -846]} />
+			<Img src={batteryIcon} width={54} position={[354, -847]} />
+			{/*
+			*/}
+
+			<Img src={httpsLockIcon} width={29} position={[-122, 690]} />
+
+
+			<Rect height={267} width={828} fill={'white'}
+				position={[0, 762]} opacity={0.9} />
+			<Rect height={90} width={80} fill={'white'} position={[-413, 689]} opacity={0.8}
+				radius={25} />
+			<Rect height={90} width={717} fill={'white'} position={[0, 689]} opacity={0.8}
+				radius={25}
+			/>
+
+			<Txt fontFamily={'SF Pro Text'} position={[22, 688]} fontSize={34} fill={'black'}>theartearoma.com</Txt>
+
+
+			{/* browser UI chin */}
+			<Img src={prevIcon} width={22} position={[-356, 784]} />
+			<Img src={nextIcon} width={22} position={[-176, 784]} />
+			<Img src={shareIcon} width={38} position={[-4, 783]} />
+			<Img src={bookmarkIcon} width={47} position={[170, 784]} />
+			<Img src={copyIcon} width={44} position={[346, 784]} />
+			<Img src={tabsIcon} width={33} position={[-306, 689]} />
+
+			<Rect height={10} width={294} fill={'black'}
+				position={[0, 875]}
+				radius={25} />
+
+			<Img src={iPhone11Mockup} height={1905} />
+		</>);
+	}
+
+}
--- src/scenes/example.tsx
+++ src/scenes/example.tsx
@@ -1,18 +1,9 @@
 import { Circle, Img, makeScene2D, Rect, Txt } from '@motion-canvas/2d';
-import { createRef } from '@motion-canvas/core';
+import { Color, createRef, easeInOutCubic, tween } from '@motion-canvas/core';
 import iPhone11Mockup from '/src/images/iphone-mockup.png';
 import iPhoneSafariExample from '/src/images/iphone-11-safari-example.jpeg';
-import batteryIcon from '/src/icons/icons8-charged-battery-100.png';
-import networkSignalIcon from '/src/icons/icons8-signal-100.png';
-import wifiSignalIcon from '/src/icons/icons8-wi-fi-100.png';
-import httpsLockIcon from '/src/icons/icons8-lock.svg';
-import heroComposition from '/src/images/hero-composition.svg';
-import prevIcon from '/src/icons/prev-icon.svg';
-import nextIcon from '/src/icons/next-icon.svg';
-import shareIcon from '/src/icons/share-icon.svg';
-import bookmarkIcon from '/src/icons/bookmark-icon.svg';
-import copyIcon from '/src/icons/copy-icon.svg';
-import tabsIcon from '/src/icons/tabs-icon.svg';
+import iPhoneSafariExample2 from '/src/images/iphone-11-safari-example2.jpeg';
+import { IPhone11SafariMockup } from '../lib/IPhone11SafariMockup/IPhone11SafariMockup';
 
 // example of how box shadows are drawn
 //
@@ -23,61 +14,31 @@ export default makeScene2D(function*(view) {
   // Create your animations here
 
   const circle = createRef<Circle>();
+  const mockup = createRef<IPhone11SafariMockup>();
 
   view.add(<>
     <Rect height={2100} width={3733} fill={'#3c3c3c'} />
-
-    {/*
-    <Img src={iPhoneSafariExample} height={1792} />
-    */}
-
-    {/* browser window */}
-    <Rect height={1792} width={828} fill={'#ffffff'}>
+    <IPhone11SafariMockup ref={mockup}>
       {/*
-    <Rect height={1792} width={828}>
-    */}
-      <Txt fontFamily={'SF Pro Text'} position={[-316, -848]} fontSize={34}>4:47</Txt>
-      <Img src={networkSignalIcon} width={28} position={[258, -848]} />
-      <Img src={wifiSignalIcon} width={30} position={[302, -846]} />
-      <Img src={batteryIcon} width={60} position={[353, -849]} />
+      <Img src={iPhoneSafariExample2} height={1792} />
 
-      <Img src={httpsLockIcon} width={29} position={[-122, 690]} />
-
-      <Img src={heroComposition} width={460} position={[20, -72]} />
-    </Rect>
-
-    <Circle ref={circle} size={320} fill={'lightseagreen'} />
-
-
-    <Rect height={267} width={828} fill={'#3c3c3c'}
-      position={[0, 762]} opacity={0.8} />
-    <Rect height={90} width={80} fill={'#3c3c3c'} position={[-413, 689]} opacity={0.3}
-      radius={25} />
-    <Rect height={90} width={717} fill={'#3c3c3c'} position={[0, 689]} opacity={0.3}
-      radius={25}
-    />
-    {/*
-    */}
-
-
-    <Rect height={10} width={294} fill={'white'}
-      position={[0, 875]}
-      radius={25} />
-    {/*
-    */}
-
-    {/* browser UI chin */}
-    <Img src={prevIcon} width={22} position={[-356, 784]} />
-    <Img src={nextIcon} width={22} position={[-176, 784]} />
-    <Img src={shareIcon} width={38} position={[-4, 783]} />
-    <Img src={bookmarkIcon} width={47} position={[170, 784]} />
-    <Img src={copyIcon} width={44} position={[346, 784]} />
-    <Img src={tabsIcon} width={33} position={[-306, 689]} />
-
-
-    <Txt fontFamily={'SF Pro Text'} position={[22, 688]} fontSize={34} fill={'white'}>theartearoma.com</Txt>
-    <Img src={iPhone11Mockup} height={1905} />
+			<Rect height={1792} width={828} fill={'white'}>
+                        </Rect>
+                        */}
+      <Circle ref={circle} size={320} fill={'lightseagreen'} />
+    </IPhone11SafariMockup>
   </>);
+
+  //yield*
+  //  tween(1, value => {
+  //    mockup().statusBarColor(
+  //      Color.lerp(
+  //        new Color('#e6a700'),
+  //        new Color('#e13238'),
+  //        easeInOutCubic(value),
+  //      ),
+  //    );
+  //  });
 
   yield* circle().scale(2, 2).to(1, 2);
 });
